04405a413a544a1c70c59ff3273364da07c17d40
luizfigueiradtp-spec <luizfigueira.dtp@gmail.com>
2026-03-06 08:47:13 -0300

    Update desafio_1003.py

--- desafio_1003.py
+++ desafio_1003.py
@@ -1,17 +1,6 @@
-"""
-Beecrowd 1003 - Soma Simples
+A = int(input())
+B = int(input())
 
-Leia dois valores inteiros, no caso para variáveis A e B.
-A seguir, calcule a soma entre elas e atribua à variável SOMA.
-A seguir escrever o valor desta variável.
+SOMA = A + B
 
-Entrada: O arquivo de entrada contém 2 valores inteiros.
-
-Saída: Imprima a mensagem "SOMA" com todas as letras maiúsculas, com um espaço
-em branco antes e depois da igualdade seguido pelo valor correspondente à soma
-de A e B.
-"""
-
-# Link do problema: https://judge.beecrowd.com/pt/problems/view/1003
-
-# Escreva sua solução abaixo
+print("SOMA =", SOMA)
