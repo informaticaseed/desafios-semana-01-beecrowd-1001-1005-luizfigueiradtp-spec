7c8296e1a0bd85a9ee7e473f89eb95c068656777
luizfigueiradtp-spec <luizfigueira.dtp@gmail.com>
2026-03-06 08:50:03 -0300

    Update desafio_1005.py

--- desafio_1005.py
+++ desafio_1005.py
@@ -1,20 +1,6 @@
-"""
-Beecrowd 1005 - Média 1
+A = float(input())
+B = float(input())
 
-Leia 2 valores de ponto flutuante de dupla precisão A e B, que correspondem a
-2 notas de um aluno. A seguir, calcule a média do aluno, sabendo que a nota A
-tem peso 3.5 e a nota B tem peso 7.5 (A soma dos pesos portanto é 11).
-Assuma que cada nota pode ir de 0 até 10.0, sempre com uma casa decimal.
+MEDIA = (A * 3.5 + B * 7.5) / 11
 
-Entrada: O arquivo de entrada contém 2 valores com uma casa decimal cada um.
-
-Saída: Imprima a mensagem "MEDIA" seguido pelo símbolo de igual e pelo valor
-da média do aluno. Obrigatoriamente deve ser impresso uma casa decimal.
-Use variáveis de dupla precisão (double) e como todos os problemas, não esqueça
-de imprimir o fim de linha após o resultado, caso contrário, você receberá
-"Presentation Error".
-"""
-
-# Link do problema: https://judge.beecrowd.com/pt/problems/view/1005
-
-# Escreva sua solução abaixo
+print(f"MEDIA = {MEDIA:.1f}")
